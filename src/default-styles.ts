5fd63d88bf966b3523f4a8eebbeea8a875542d27
Bas Doppen <basdoppen@Bass-MacBook.local>
2017-10-27 11:53:33 +0200

    styling semantic suggestions

---
+++ src/default-styles.ts
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+const fontStyle: React.CSSProperties = {
+    fontFamily: "'Roboto', sans-serif",
+    fontSize: '18px',
+	fontWeight: 300,
+    lineHeight: '150%',
+}
+
+export { fontStyle };
+
+
+const grayLightBackground: React.CSSProperties = {
+    backgroundColor: '#f9f9f9',
+}
+
+export { grayLightBackground };
