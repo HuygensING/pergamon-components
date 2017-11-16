ec19cfc3ed303c68becc4bf54b4d9dc3a9266411
Gijsjan <agijsbro@gmail.com>
2017-11-16 10:02:25 +0100

    Change ternary operator and fix indentation

--- build/annotation-list/annotation.js
+++ build/annotation-list/annotation.js
@@ -5,10 +5,24 @@ const annotation_form_1 = require("./annotation-form");
 const index_1 = require("../rendered-text/index");
 const default_styles_1 = require("../default-styles");
 const Annotation = (props) => React.createElement("li", { style: { minHeight: '2em' } },
-    React.createElement("h4", { onClick: () => props.activateAnnotation(props.annotation), style: Object.assign({}, default_styles_1.fontStyle, default_styles_1.blueFontStyle) },
+    React.createElement("h4", { onClick: () => props.activateAnnotation(props.annotation), style: Object.assign({}, default_styles_1.fontStyle) },
+        props.annotation.type !== 'persName' &&
+            React.createElement("div", { style: { color: '#444', fontSize: '.85em' } }, "34"),
+        props.annotation.type === 'persName' &&
+            React.createElement("img", { style: {
+                    width: "12px",
+                    height: 'auto',
+                    marginRight: '.2em',
+                }, src: "http://design.huygens.knaw.nl/static/icons/person.svg" }),
+        props.annotation.type === 'placeName' &&
+            React.createElement("img", { style: {
+                    width: "12px",
+                    height: 'auto',
+                    marginRight: '.2em',
+                }, src: "http://design.huygens.knaw.nl/static/icons/location.svg" }),
         props.rootAnnotation.text.slice(props.annotation.start, props.annotation.end),
-        props.annotation.type === 'persName' ?
-            React.createElement("small", { style: { color: '#444', marginLeft: '1em' } }, "(xxxx - xxxx)") : null),
+        props.annotation.type === 'persName' &&
+            React.createElement("small", { style: { color: '#444', marginLeft: '1em' } }, "(xxxx - xxxx)")),
     (props.activeAnnotation != null &&
         props.annotation.id === props.activeAnnotation.id) ?
         React.createElement(annotation_form_1.default, { activeAnnotation: props.activeAnnotation, rootAnnotation: props.rootAnnotation }) :
--- src/annotation-list/annotation.tsx
+++ src/annotation-list/annotation.tsx
@@ -25,35 +25,36 @@ const Annotation: React.SFC<IAnnotationProps> = (props) =>
 
 			}}
 		>
-		{
-			props.annotation.type != 'persName' ?
-				<div style={{ color: '#444', fontSize:'.85em'}}>34</div> : null
-		}
-		{
-			props.annotation.type === 'persName' ?
-			<img
-				style={{
-					width: "12px",
-					height:'auto',
-					marginRight: '.2em',
-				}}
-				src="http://design.huygens.knaw.nl/static/icons/person.svg" /> : null
-		}
-		{
-			props.annotation.type === 'placeName' ?
-			<img
-				style={{
-					width: "12px",
-					height:'auto',
-					marginRight: '.2em',
-				}}
-				src="http://design.huygens.knaw.nl/static/icons/location.svg" /> : null
-		}
-
-			{props.rootAnnotation.text.slice(props.annotation.start, props.annotation.end)}
 			{
-				props.annotation.type === 'persName' ?
-					<small style={{ color: '#444', marginLeft: '1em' }}>(xxxx - xxxx)</small> : null
+				props.annotation.type !== 'persName' &&
+				<div style={{ color: '#444', fontSize:'.85em'}}>34</div>
+			}
+			{
+				props.annotation.type === 'persName' &&
+				<img
+					style={{
+						width: "12px",
+						height:'auto',
+						marginRight: '.2em',
+					}}
+					src="http://design.huygens.knaw.nl/static/icons/person.svg"
+				/>
+			}
+			{
+				props.annotation.type === 'placeName' &&
+				<img
+					style={{
+						width: "12px",
+						height:'auto',
+						marginRight: '.2em',
+					}}
+					src="http://design.huygens.knaw.nl/static/icons/location.svg"
+				/>
+			}
+			{ props.rootAnnotation.text.slice(props.annotation.start, props.annotation.end) }
+			{
+				props.annotation.type === 'persName' &&
+				<small style={{ color: '#444', marginLeft: '1em' }}>(xxxx - xxxx)</small>
 			}
 		</h4>
 		{
