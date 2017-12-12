c161f76807b792dc680fbe2da68feddabcf40286
Gijsjan <agijsbro@gmail.com>
2017-12-12 13:44:07 +0100

    Storybook: use set in rootAnnotation

--- .storybook/data.ts
+++ .storybook/data.ts
@@ -3,346 +3,346 @@ import Annotation from '../src/models/annotation'
 
 export const activeAnnotation: Annotation = new Annotation({
 	"attributes": new Map()
-    .set("key", "PE00873")
-    .set("resp", "#ckcc")
-    .set("resp_key", "#ckcc")
-  ,
+		.set("key", "PE00873")
+		.set("resp", "#ckcc")
+		.set("resp_key", "#ckcc")
+	,
 	"id": "AV8Fr6Y2zCK540LqWxJ_",
 	"start": 596,
 	"end": 606,
 	"type": "persName",
-  "target": "AV8Fr6BvzCK540LqWxIx",
-  "text": "",
+	"target": "AV8Fr6BvzCK540LqWxIx",
+	"text": "",
 	"source": 'xml' 
 })
 
 const toAnnotation = (root) => {
-  if (root.hasOwnProperty('annotations')) {
-    root.annotations = root.annotations.map(toAnnotation)
-  }
-  return new Annotation(root)
+	if (root.hasOwnProperty('annotations')) {
+		root.annotations = root.annotations.map(toAnnotation)
+	}
+	return new Annotation(root)
 }
 export const rootAnnotation = toAnnotation({ 
-  "annotations": [
-    {
-      "attributes": {
-        "type": "sender",
-        "value": "PE00315"
-      },
-      "id": "AV8Fr6I9zCK540LqWxJH",
-      "start": 5,
-      "end": 5,
-      "type": "meta",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "text": "",
-      "source": "xml"
-    },
-    {
-      "attributes": {
-        "lang": "la",
-        "xml:id": "text-1"
-      },
-      "id": "AV8Fr6OjzCK540LqWxJd",
-      "start": 11,
-      "end": 1338,
-      "type": "text",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "text": "",
-      "source": "xml"
-    },
-    {
-      "id": "AV8Fr6QJzCK540LqWxJh",
-      "start": 12,
-      "end": 1337,
-      "type": "body",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "text": "",
-      "source": "xml"
-    },
-    {
-      "attributes": {
-        "type": "letter",
-        "xml:id": "div-1"
-      },
-      "id": "AV8Fr6RmzCK540LqWxJo",
-      "start": 13,
-      "end": 1336,
-      "type": "div",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "text": "",
-      "source": "xml"
-    },
-	  activeAnnotation,
-    {
-      "attributes": {
-        "resp": "#ckcc"
-      },
-      "id": "AV8Fr6X8zCK540LqWxJ-",
-      "start": 256,
-      "end": 264,
-      "type": "persName",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "text": "",
-      "source": "xml"
-    },
-    {
-      "attributes": {
-        "key": "PE01864",
-        "resp": "#ckcc",
-        "resp_key": "#ckcc"
-      },
-      "id": "AV8Fr6Z1zCK540LqWxKE",
-      "start": 750,
-      "end": 766,
-      "type": "persName",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "text": "",
-      "source": "xml"
-    },
-    {
-      "attributes": {
-        "lang": "la",
-        "type": "closer",
-        "xml:id": "p-3"
-      },
-      "id": "AV8Fr6bkzCK540LqWxKJ",
-      "start": 1250,
-      "end": 1335,
-      "text": "",
-      "type": "p",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "source": "xml"
-    },
-    {
-      "attributes": {
-        "key": "PL01453",
-        "resp": "#ckcc",
-        "resp_key": "#ckcc"
-      },
-      "id": "AV8Fr6dGzCK540LqWxKO",
-      "start": 1308,
-      "end": 1318,
-      "type": "placeName",
-      "text": "",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "source": "xml"
-    },
-    {
-      "attributes": {
-        "resp": "#ckcc"
-      },
-      "id": "AV8Fr6fGzCK540LqWxKX",
-      "start": 1319,
-      "end": 1334,
-      "type": "date",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "text": "",
-      "source": "xml"
-    },
-    {
-      "id": "AV8Fr6D1zCK540LqWxI3",
-      "start": 0,
-      "end": 1339,
-      "type": "TEI",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "text": "",
-      "source": "xml"
-    },
-    {
-      "attributes": {
-        "type": "uuid",
-        "value": "39178b19-e953-4b22-a2c4-11a554d673b8"
-      },
-      "id": "AV8Fr6GIzCK540LqWxI8",
-      "start": 2,
-      "end": 2,
-      "type": "meta",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "text": "",
-      "source": "xml"
-    },
-    {
-      "attributes": {
-        "type": "senderloc",
-        "value": "PL01453"
-      },
-      "id": "AV8Fr6KxzCK540LqWxJP",
-      "start": 6,
-      "end": 6,
-      "type": "meta",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "text": "",
-      "source": "xml"
-    },
-    {
-      "attributes": {
-        "type": "recipientloc",
-        "value": "PL01325"
-      },
-      "id": "AV8Fr6M2zCK540LqWxJW",
-      "start": 8,
-      "end": 8,
-      "type": "meta",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "text": "",
-      "source": "xml"
-    },
-    {
-      "attributes": {
-        "resp": "#ckcc"
-      },
-      "id": "AV8Fr6WizCK540LqWxJ4",
-      "start": 34,
-      "end": 41,
-      "type": "persName",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "text": "",
-      "source": "xml"
-    },
-    {
-      "attributes": {
-        "key": "PE02036",
-        "resp": "#ckcc",
-        "resp_key": "#ckcc"
-      },
-      "id": "AV8Fr6cLzCK540LqWxKL",
-      "start": 1259,
-      "end": 1265,
-      "type": "persName",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "text": "",
-      "source": "xml"
-    },
-    {
-      "attributes": {
-        "key": "PE02036",
-        "resp": "#ckcc",
-        "resp_key": "#ckcc"
-      },
-      "id": "AV8Fr6UZzCK540LqWxJx",
-      "start": 26,
-      "end": 32,
-      "type": "persName",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "text": "",
-      "source": "xml"
-    },
-    {
-      "attributes": {
-        "lang": "la",
-        "r": "1.262",
-        "xml:id": "p-2"
-      },
-      "id": "AV8Fr6VbzCK540LqWxJ0",
-      "start": 34,
-      "end": 1249,
-      "type": "p",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "text": "",
-      "source": "xml"
-    },
-    {
-      "attributes": {
-        "type": "id",
-        "value": "0056"
-      },
-      "id": "AV8Fr6GnzCK540LqWxI_",
-      "start": 3,
-      "end": 3,
-      "type": "meta",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "text": "",
-      "source": "xml"
-    },
-    {
-      "attributes": {
-        "type": "recipient",
-        "value": "PE02036"
-      },
-      "id": "AV8Fr6MMzCK540LqWxJS",
-      "start": 7,
-      "end": 7,
-      "type": "meta",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "text": "",
-      "source": "xml"
-    },
-    {
-      "attributes": {
-        "type": "language",
-        "value": "la"
-      },
-      "id": "AV8Fr6OBzCK540LqWxJZ",
-      "start": 9,
-      "end": 9,
-      "type": "meta",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "text": "",
-      "source": "xml"
-    },
-    {
-      "attributes": {
-        "lang": "la",
-        "type": "opener",
-        "xml:id": "p-1"
-      },
-      "id": "AV8Fr6ThzCK540LqWxJt",
-      "start": 14,
-      "end": 33,
-      "type": "p",
-      "text": "",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "source": "xml"
-    },
-    {
-      "attributes": {
-        "key": "PE02864",
-        "resp": "#ckcc",
-        "resp_key": "#ckcc"
-      },
-      "id": "AV8Fr6akzCK540LqWxKG",
-      "start": 769,
-      "end": 781,
-      "type": "persName",
-      "text": "",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "source": "xml"
-    },
-    {
-      "id": "AV8Fr6EwzCK540LqWxI6",
-      "start": 1,
-      "end": 10,
-      "type": "teiHeader",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "text": "",
-      "source": "xml"
-    },
-    {
-      "attributes": {
-        "type": "date",
-        "value": "1627-06-27"
-      },
-      "id": "AV8Fr6IBzCK540LqWxJD",
-      "start": 4,
-      "end": 4,
-      "type": "meta",
-      "target": "AV8Fr6BvzCK540LqWxIx",
-      "text": "",
-      "source": "xml"
-    }
-  ],
-  "end": 1339,
-  "id": "AV8Fr6BvzCK540LqWxIx",
-  "metadata": {
-    "sender": "Piet Plezier",
-    "senderloc": "Enkhuizen",
-    "recipient": "Andre van Duin",
-    "recipientloc": "Bonaire",
-    "date": "2011-04-18",
-  },
-  "source": "xml",
-  "start": 0,
-  "target": null,
-  "text": "nnnnnnnnnnnnnnNobilissime Hugeni,nBacchum olim antiquitas laetitiae datorem dixit, coluitque, ego Te. Adeo beneficium beneficio, gaudium gaudio cumulas: ut jam de mutando foro cogitem, cum rationibus subductis non videam, qua me via tuo aere extricem. Cum Aeschine meipsum offero, pene dixissem vernam, imo mediastinum: nisi, quae tua est humanitas, manumittendos esse putes poetas, vel propter mediocre ingenium. Quod si nolis, age, vel pedes meos creta alba nota, Romanorum more; vel Iudaeorum ritu aurem mihi pertunde, ut me non mei amplius, sed tui juris esse publice constet. Illustrissimum Carletonum intra biduum aut triduum Scazonte compellabo, eique adventum in Batavis gratulabor, unaque pro perlato ad me regio munere gratias agam. Postea Penbrociae Comes & Oldisworthis luculentiore vel carmine vel epistola compellandi erunt. Verum, quod regiam istam liberalitatem silentio premi cupis, gravate obsequar. Citius ringar, quam ut insignem Regis summi munificentiam, & ingentia tua erga me merita ingrato silentio involvam. Quid enim? vis me cogitasse, quid optandum fuerit, non quid impetratum sit? vis me aestimasse beneficium, non benefactorem? praeterquam quod sciri expediat, aliquod quoque precium esse studiorum meorum apud exteros.nVale, mi Hugeni & Patriae, Principi, tuisque charus vive. Lugd. Bat. Cal. Maji, 1627.nnnn",
-  "type": "__root"
+	"annotations": [
+		{
+			"attributes": {
+				"type": "sender",
+				"value": "PE00315"
+			},
+			"id": "AV8Fr6I9zCK540LqWxJH",
+			"start": 5,
+			"end": 5,
+			"type": "meta",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"text": "",
+			"source": "xml"
+		},
+		{
+			"attributes": {
+				"lang": "la",
+				"xml:id": "text-1"
+			},
+			"id": "AV8Fr6OjzCK540LqWxJd",
+			"start": 11,
+			"end": 1338,
+			"type": "text",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"text": "",
+			"source": "xml"
+		},
+		{
+			"id": "AV8Fr6QJzCK540LqWxJh",
+			"start": 12,
+			"end": 1337,
+			"type": "body",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"text": "",
+			"source": "xml"
+		},
+		{
+			"attributes": {
+				"type": "letter",
+				"xml:id": "div-1"
+			},
+			"id": "AV8Fr6RmzCK540LqWxJo",
+			"start": 13,
+			"end": 1336,
+			"type": "div",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"text": "",
+			"source": "xml"
+		},
+		activeAnnotation,
+		{
+			"attributes": {
+				"resp": "#ckcc"
+			},
+			"id": "AV8Fr6X8zCK540LqWxJ-",
+			"start": 256,
+			"end": 264,
+			"type": "persName",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"text": "",
+			"source": "xml"
+		},
+		{
+			"attributes": {
+				"key": "PE01864",
+				"resp": "#ckcc",
+				"resp_key": "#ckcc"
+			},
+			"id": "AV8Fr6Z1zCK540LqWxKE",
+			"start": 750,
+			"end": 766,
+			"type": "persName",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"text": "",
+			"source": "xml"
+		},
+		{
+			"attributes": {
+				"lang": "la",
+				"type": "closer",
+				"xml:id": "p-3"
+			},
+			"id": "AV8Fr6bkzCK540LqWxKJ",
+			"start": 1250,
+			"end": 1335,
+			"text": "",
+			"type": "p",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"source": "xml"
+		},
+		{
+			"attributes": {
+				"key": "PL01453",
+				"resp": "#ckcc",
+				"resp_key": "#ckcc"
+			},
+			"id": "AV8Fr6dGzCK540LqWxKO",
+			"start": 1308,
+			"end": 1318,
+			"type": "placeName",
+			"text": "",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"source": "xml"
+		},
+		{
+			"attributes": {
+				"resp": "#ckcc"
+			},
+			"id": "AV8Fr6fGzCK540LqWxKX",
+			"start": 1319,
+			"end": 1334,
+			"type": "date",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"text": "",
+			"source": "xml"
+		},
+		{
+			"id": "AV8Fr6D1zCK540LqWxI3",
+			"start": 0,
+			"end": 1339,
+			"type": "TEI",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"text": "",
+			"source": "xml"
+		},
+		{
+			"attributes": {
+				"type": "uuid",
+				"value": "39178b19-e953-4b22-a2c4-11a554d673b8"
+			},
+			"id": "AV8Fr6GIzCK540LqWxI8",
+			"start": 2,
+			"end": 2,
+			"type": "meta",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"text": "",
+			"source": "xml"
+		},
+		{
+			"attributes": {
+				"type": "senderloc",
+				"value": "PL01453"
+			},
+			"id": "AV8Fr6KxzCK540LqWxJP",
+			"start": 6,
+			"end": 6,
+			"type": "meta",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"text": "",
+			"source": "xml"
+		},
+		{
+			"attributes": {
+				"type": "recipientloc",
+				"value": "PL01325"
+			},
+			"id": "AV8Fr6M2zCK540LqWxJW",
+			"start": 8,
+			"end": 8,
+			"type": "meta",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"text": "",
+			"source": "xml"
+		},
+		{
+			"attributes": {
+				"resp": "#ckcc"
+			},
+			"id": "AV8Fr6WizCK540LqWxJ4",
+			"start": 34,
+			"end": 41,
+			"type": "persName",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"text": "",
+			"source": "xml"
+		},
+		{
+			"attributes": {
+				"key": "PE02036",
+				"resp": "#ckcc",
+				"resp_key": "#ckcc"
+			},
+			"id": "AV8Fr6cLzCK540LqWxKL",
+			"start": 1259,
+			"end": 1265,
+			"type": "persName",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"text": "",
+			"source": "xml"
+		},
+		{
+			"attributes": {
+				"key": "PE02036",
+				"resp": "#ckcc",
+				"resp_key": "#ckcc"
+			},
+			"id": "AV8Fr6UZzCK540LqWxJx",
+			"start": 26,
+			"end": 32,
+			"type": "persName",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"text": "",
+			"source": "xml"
+		},
+		{
+			"attributes": {
+				"lang": "la",
+				"r": "1.262",
+				"xml:id": "p-2"
+			},
+			"id": "AV8Fr6VbzCK540LqWxJ0",
+			"start": 34,
+			"end": 1249,
+			"type": "p",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"text": "",
+			"source": "xml"
+		},
+		{
+			"attributes": {
+				"type": "id",
+				"value": "0056"
+			},
+			"id": "AV8Fr6GnzCK540LqWxI_",
+			"start": 3,
+			"end": 3,
+			"type": "meta",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"text": "",
+			"source": "xml"
+		},
+		{
+			"attributes": {
+				"type": "recipient",
+				"value": "PE02036"
+			},
+			"id": "AV8Fr6MMzCK540LqWxJS",
+			"start": 7,
+			"end": 7,
+			"type": "meta",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"text": "",
+			"source": "xml"
+		},
+		{
+			"attributes": {
+				"type": "language",
+				"value": "la"
+			},
+			"id": "AV8Fr6OBzCK540LqWxJZ",
+			"start": 9,
+			"end": 9,
+			"type": "meta",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"text": "",
+			"source": "xml"
+		},
+		{
+			"attributes": {
+				"lang": "la",
+				"type": "opener",
+				"xml:id": "p-1"
+			},
+			"id": "AV8Fr6ThzCK540LqWxJt",
+			"start": 14,
+			"end": 33,
+			"type": "p",
+			"text": "",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"source": "xml"
+		},
+		{
+			"attributes": {
+				"key": "PE02864",
+				"resp": "#ckcc",
+				"resp_key": "#ckcc"
+			},
+			"id": "AV8Fr6akzCK540LqWxKG",
+			"start": 769,
+			"end": 781,
+			"type": "persName",
+			"text": "",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"source": "xml"
+		},
+		{
+			"id": "AV8Fr6EwzCK540LqWxI6",
+			"start": 1,
+			"end": 10,
+			"type": "teiHeader",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"text": "",
+			"source": "xml"
+		},
+		{
+			"attributes": {
+				"type": "date",
+				"value": "1627-06-27"
+			},
+			"id": "AV8Fr6IBzCK540LqWxJD",
+			"start": 4,
+			"end": 4,
+			"type": "meta",
+			"target": "AV8Fr6BvzCK540LqWxIx",
+			"text": "",
+			"source": "xml"
+		}
+	],
+	"end": 1339,
+	"id": "AV8Fr6BvzCK540LqWxIx",
+	"metadata": new Map()
+		.set("sender", "Piet Plezier")
+		.set("senderloc", "Enkhuizen")
+		.set("recipient", "Andre van Duin")
+		.set("recipientloc", "Bonaire")
+		.set("date", "2011-04-18")
+	,
+	"source": "xml",
+	"start": 0,
+	"target": null,
+	"text": "nnnnnnnnnnnnnnNobilissime Hugeni,nBacchum olim antiquitas laetitiae datorem dixit, coluitque, ego Te. Adeo beneficium beneficio, gaudium gaudio cumulas: ut jam de mutando foro cogitem, cum rationibus subductis non videam, qua me via tuo aere extricem. Cum Aeschine meipsum offero, pene dixissem vernam, imo mediastinum: nisi, quae tua est humanitas, manumittendos esse putes poetas, vel propter mediocre ingenium. Quod si nolis, age, vel pedes meos creta alba nota, Romanorum more; vel Iudaeorum ritu aurem mihi pertunde, ut me non mei amplius, sed tui juris esse publice constet. Illustrissimum Carletonum intra biduum aut triduum Scazonte compellabo, eique adventum in Batavis gratulabor, unaque pro perlato ad me regio munere gratias agam. Postea Penbrociae Comes & Oldisworthis luculentiore vel carmine vel epistola compellandi erunt. Verum, quod regiam istam liberalitatem silentio premi cupis, gravate obsequar. Citius ringar, quam ut insignem Regis summi munificentiam, & ingentia tua erga me merita ingrato silentio involvam. Quid enim? vis me cogitasse, quid optandum fuerit, non quid impetratum sit? vis me aestimasse beneficium, non benefactorem? praeterquam quod sciri expediat, aliquod quoque precium esse studiorum meorum apud exteros.nVale, mi Hugeni & Patriae, Principi, tuisque charus vive. Lugd. Bat. Cal. Maji, 1627.nnnn",
+	"type": "__root"
 })
