debcc870464369143d2084e18657886b9a9f7b22
Gijsjan <agijsbro@gmail.com>
2017-12-14 14:16:13 +0100

    Change label of unidentified named entities

--- build/annotation-list/error-label.js
+++ build/annotation-list/error-label.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const React = require("react");
 const ErrorLabel = (props) => React.createElement("div", { style: { display: 'inline-block' } },
     React.createElement("div", null, props.rootAnnotation.text.slice(props.annotation.start, props.annotation.end)),
-    React.createElement("div", { style: { color: 'red', fontSize: '.8em' } }, "No `text` attribute found!"));
+    React.createElement("div", { style: { color: 'red', fontSize: '.8em' } }, "Not identified"));
 exports.default = ErrorLabel;
--- src/annotation-list/error-label.tsx
+++ src/annotation-list/error-label.tsx
@@ -4,7 +4,7 @@ import { IProps } from './label'
 const ErrorLabel: React.SFC<IProps> = (props) =>
 	<div style={{ display: 'inline-block' }}>
 		<div>{props.rootAnnotation.text.slice(props.annotation.start, props.annotation.end)}</div>
-		<div style={{ color: 'red', fontSize: '.8em' }}>No `text` attribute found!</div>
+		<div style={{ color: 'red', fontSize: '.8em' }}>Not identified</div>
 	</div>
 
 export default ErrorLabel
